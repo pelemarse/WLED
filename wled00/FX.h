b169e117c07e68cb069b0ca3dc05a2580464bd40
pelemarse <signal@pelemarse.com>
2025-04-17 05:26:32 +0300

    Reorder MODE_COUNT in FX.h to precede wipe effects

--- wled00/FX.h
+++ wled00/FX.h
@@ -353,10 +353,11 @@ extern byte realtimeMode;           // used in getMappedPixelIndex()
 #define FX_MODE_PS1DSONICSTREAM        214
 #define FX_MODE_PS1DSONICBOOM          215
 #define FX_MODE_PS1DSPRINGY            216
-#define FX_MODE_WIPE_UP_LOOP           217
-#define FX_MODE_WIPE_UP_ONCE           218
-#define FX_MODE_WIPE_DOWN_ONCE         219
-#define MODE_COUNT                     220
+#define MODE_COUNT                     217
+#define FX_MODE_WIPE_UP_LOOP           218
+#define FX_MODE_WIPE_UP_ONCE           219
+#define FX_MODE_WIPE_DOWN_ONCE         220
+
 
 
 #define BLEND_STYLE_FADE            0x00  // universal
