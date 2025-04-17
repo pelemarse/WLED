d5ce54fcdb5ef721f51edfe45925c4986588a2eb
pelemarse <signal@pelemarse.com>
2025-04-17 03:36:22 +0300

    Add Wipe Up Once effect

--- wled00/FX.cpp
+++ wled00/FX.cpp
@@ -309,6 +309,36 @@ uint16_t mode_wipe_up_loop(void) {
 static const char _data_FX_MODE_WIPE_UP_LOOP[] PROGMEM = "Wipe Up@!,!;!,!;!";
 
 
+// Wipe up effect - fills LEDs one after another, then stays filled
+uint16_t mode_wipe_up_once(void) {
+  if (SEGLEN <= 1) return mode_static();
+
+  uint32_t cycleTime = 750 + (255 - SEGMENT.speed)*150;
+  uint32_t perc = strip.now % cycleTime;
+  unsigned prog = (perc * 65535) / cycleTime;
+
+  // Map progress to pixel position
+  unsigned fill = (prog * SEGLEN) / 32768;
+
+  // If we've reached the end, keep all pixels filled
+  if (fill >= SEGLEN) {
+    SEGMENT.fill(SEGMENT.color_from_palette(0, true, PALETTE_SOLID_WRAP, 0));
+    return FRAMETIME;
+  }
+
+  // Fill pixels progressively
+  for (unsigned i = 0; i < SEGLEN; i++) {
+    if (i <= fill) {
+      SEGMENT.setPixelColor(i, SEGMENT.color_from_palette(i, true, PALETTE_SOLID_WRAP, 0));
+    } else {
+      SEGMENT.setPixelColor(i, SEGCOLOR(1));
+    }
+  }
+
+  return FRAMETIME;
+}
+static const char _data_FX_MODE_WIPE_UP_ONCE[] PROGMEM = "Wipe Up Once@!,!;!,!;!";
+
 /*
  * Random color introduced alternating from start and end of strip.
  */
@@ -10484,6 +10514,7 @@ void WS2812FX::setupEffectData() {
   addEffect(FX_MODE_COLOR_WIPE, &mode_color_wipe, _data_FX_MODE_COLOR_WIPE);
   addEffect(FX_MODE_COLOR_WIPE_RANDOM, &mode_color_wipe_random, _data_FX_MODE_COLOR_WIPE_RANDOM);
   addEffect(FX_MODE_WIPE_UP_LOOP, &mode_wipe_up_loop, _data_FX_MODE_WIPE_UP_LOOP);
+  addEffect(FX_MODE_WIPE_UP_ONCE, &mode_wipe_up_once, _data_FX_MODE_WIPE_UP_ONCE);
   addEffect(FX_MODE_RANDOM_COLOR, &mode_random_color, _data_FX_MODE_RANDOM_COLOR);
   addEffect(FX_MODE_COLOR_SWEEP, &mode_color_sweep, _data_FX_MODE_COLOR_SWEEP);
   addEffect(FX_MODE_DYNAMIC, &mode_dynamic, _data_FX_MODE_DYNAMIC);
--- wled00/FX.h
+++ wled00/FX.h
@@ -354,7 +354,8 @@ extern byte realtimeMode;           // used in getMappedPixelIndex()
 #define FX_MODE_PS1DSONICBOOM          215
 #define FX_MODE_PS1DSPRINGY            216
 #define FX_MODE_WIPE_UP_LOOP           217
-#define MODE_COUNT                     218
+#define FX_MODE_WIPE_UP_ONCE           218
+#define MODE_COUNT                     219
 
 
 #define BLEND_STYLE_FADE            0x00  // universal
